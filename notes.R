3d870a48ae877e7a9d4fa33efcbfee0a11890aba
woodcreeper <david.lapuma@celltracktech.com>
2020-07-02 00:47:42 -0400

    general updates

--- notes.R
+++ notes.R
@@ -6,3 +6,11 @@ Target:
   
 Reference:
   see [Appendix I](#appendix_2).
+    
+    
+    
+Themes
+default, cerulean, journal, flatly, darkly, readable, spacelab, united, cosmo, lumen, paper, sandstone, simplex, and yeti
+
+Highlight
+highlight specifies the syntax highlighting style. Supported styles include default, tango, pygments, kate, monochrome, espresso, zenburn, haddock, breezedark, and textmate. Pass null to prevent syntax highlighting.
